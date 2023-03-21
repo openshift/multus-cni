c39989783f16fd1f5012b5e841ddd2ce3c980336
nicklesimba <simha.nikhil@gmail.com>
2023-03-21 17:24:24 -0500

    added logging comment
    Signed-off-by: nicklesimba <simha.nikhil@gmail.com>

--- images/entrypoint.sh
+++ images/entrypoint.sh
@@ -490,7 +490,7 @@ if [ "$MULTUS_CLEANUP_CONFIG_ON_EXIT" == true ]; then
     svcaccountsum=$(md5sum $SERVICE_ACCOUNT_TOKEN_PATH | awk '{print $1}')
     casum=$(md5sum $KUBE_CA_FILE | awk '{print $1}')
     if [ "$svcaccountsum" != "$LAST_SERVICEACCOUNT_MD5SUM" ] || [ "$casum" != "$LAST_KUBE_CA_FILE_MD5SUM" ]; then
-      # log "Detected service account or CA file change, regenerating kubeconfig..."
+      log "Detected service account or CA file change, regenerating kubeconfig..."
       generateKubeConfig
     fi
 
